833eede452bc2428b2fd5714ef7787f516689f17
Crayon <crayon1@rocketmail.com>
2008-03-21 17:26:55 +0000

    [CHG] Compression de l'exécutable [CHG] Copyright 2008

--- Build.bat
+++ Build.bat
@@ -1,9 +1,14 @@
 @echo off
 set BDS=C:\Program Files\CodeGear\RAD Studio\5.0
 set path=%path%;C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727
+set path=%path%;D:\Programmes
 
 echo Compiling code...
 msbuild Color.cbproj /t:Build /p:config="Release Build" /nologo /v:q
+if errorlevel 1 goto error
+
+echo Compressing executable...
+upx --best --force -qq color.exe
 if errorlevel 1 goto error
 
 echo Build successful.
