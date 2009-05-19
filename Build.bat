bf3660ef6767e92d6f056802e9e8ad39c6124ed4
Crayon <crayon1@rocketmail.com>
2009-05-19 04:16:36 +0000

    [CHG] Copyright 2009

--- Build.bat
+++ Build.bat
@@ -1,14 +1,15 @@
 @echo off
-set BDS=C:\Program Files\CodeGear\RAD Studio\5.0
+set BDS=C:\Program Files\CodeGear\RAD Studio\6.0
 set path=%path%;C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727
 set path=%path%;D:\Programmes
 
 echo Compiling code...
-msbuild Color.cbproj /t:Build /p:config="Release Build" /nologo /v:q
+msbuild Color.cbproj /t:Build /p:"Config=Release Build" /nologo /v:q
 if errorlevel 1 goto error
 
 echo Compressing executable...
-upx --best --force -qq color.exe
+rem upx --best --force -qq .\Release_Build\color.exe
+BCBStriper .\Release_Build\color.exe
 if errorlevel 1 goto error
 
 echo Build successful.
