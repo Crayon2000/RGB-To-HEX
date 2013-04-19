9ed43b32a6cbf562e61c369197d05ce19da62470
Crayon <crayon1@rocketmail.com>
2013-04-19 01:14:44 +0000

    [CHG] L'application peut maintenant être distribué en version 64-bit

--- Build.bat
+++ Build.bat
@@ -7,10 +7,19 @@ echo Compiling Win32 code...
 msbuild Color.cbproj /target:Build /property:"Config=Release";"Platform=Win32" /nologo /v:q
 if errorlevel 1 goto error
 
+echo Compiling Win64 code...
+msbuild Color.cbproj /target:Build /property:"Config=Release";"Platform=Win64" /nologo /v:q
+if errorlevel 1 goto error
+
 echo Compressing Win32 executable...
 rem upx --best --force -qq .\Win32\Release\color.exe
 BCBStriper .\Win32\Release\color.exe
 if errorlevel 1 goto error
+
+rem echo Compressing Win64 executable...
+rem upx --best --force -qq .\Win64\Release\color.exe
+rem BCBStriper .\Win32\Release\color.exe
+rem if errorlevel 1 goto error
 
 echo Build successful.
 goto end
