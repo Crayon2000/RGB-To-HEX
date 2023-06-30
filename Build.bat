e7c5a434e55b6f59c2386fb5195813053cd1a8f6
Crayon <crayon1@rocketmail.com>
2023-06-30 13:23:06 -0400

    Update project file to C++Builder 11

--- Build.bat
+++ Build.bat
@@ -1,7 +1,6 @@
 @echo off
-set BDS=C:\Program Files (x86)\Embarcadero\Studio\20.0
+set BDS=C:\Program Files (x86)\Embarcadero\Studio\22.0
 set path=%path%;C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319
-set path=%path%;E:\Programmes
 
 echo Compiling Win32 code...
 msbuild Color.cbproj /target:Build /property:"Config=Release";"Platform=Win32" /nologo /v:q
@@ -9,16 +8,6 @@ if errorlevel 1 goto error
 
 echo Compiling Win64 code...
 msbuild Color.cbproj /target:Build /property:"Config=Release";"Platform=Win64" /nologo /v:q
-if errorlevel 1 goto error
-
-echo Compressing Win32 executable...
-rem upx --best --force -qq .\Win32\Release\color.exe
-BCBStriper .\Win32\Release\color.exe
-if errorlevel 1 goto error
-
-echo Compressing Win64 executable...
-upx --best --force -qq .\Win64\Release\color.exe
-rem BCBStriper .\Win32\Release\color.exe
 if errorlevel 1 goto error
 
 echo Build successful.
